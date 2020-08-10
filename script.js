008fe08008a8cd6247c6499d3e5ed7ca93f930d2
RoyceAroc <47615786+RoyceAroc@users.noreply.github.com>
2020-08-09 23:29:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,15 @@
   */
   if((200 -scroll) > 85) {
   	document.getElementById("elem_fix").innerHTML = ("<polyline points=\"0,0 0," + (200-scroll) + " " + (200-scroll) + "," + (200-scroll) + " 300,90 12241,150 12241,0\" style=\"fill: #485D71;stroke:white;stroke-width:4\"/>");
+	  document.getElementById("elem_fix").style.height = "250";
   } else {
 	document.getElementById("elem_fix").innerHTML = ("<polyline points=\"0,0 0,90  300, 90 12241,150 12241,0\" style=\"fill: #485D71;stroke:white;stroke-width:4\" />");
+	 document.getElementById("elem_fix").style.height = "100";
+  }
+  if(scroll > 270) {
+	 $( "#searchBar" ).fadeIn(800);
+  } else {
+	 $( "#searchBar" ).fadeOut(100);
   }
 
 })
@@ -143,3 +150,62 @@ window.onload = function() {
 	$("#title").slideUp();
 	$("#title").slideDown(1500);
 };
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + 300 + "px";
+    } 
+  });
+}
+
+let index = [49, 103,111,265,87,127,163,241,97,0,0,249,257,275,309,317,75,169,201,233,285,119,145,183,301,0,155,295,41,67,137,175,209,217,223,59,193]
+	function open1(numA) {
+		//var browse = document.getElementById("opened");
+		$("#opened").fadeIn();
+		document.getElementById('guidelines');
+		document.getElementById('guidelines').src = "https://tsa.roydero.com/media/CompGuidelines.pdf#page=" + index[numA];
+
+		
+	}
+
+$(document).ready(function () {
+  $('li').click(function () {
+	let choose = $(this).attr('id');
+    if ($(this).hasClass('expand')) {
+      $(this).removeClass('expand');
+      $(this).find('div').text('');
+    } else {
+      // Remove all expand classes from list items
+      $('li').removeClass('expand');
+      $('li').find('div').text('');
+      // Add to the newly clicked list item
+      $(this).addClass('expand');
+      $(this).find('div').html(function () {
+        list = "https://tsa.roydero.com/images/slides/" + choose + ".jpg";
+        return "<ol class=\"clean\"><img src=\"" + list + "\"/><br><br><button onclick=\"open1(" + choose + ")\"> Read More </button></ol>";
+      });
+    }
+  });
+
+  $('.expand').click(function () {
+    // Remove all expand classes from list items
+    $('li').removeClass('expand');
+  });
+});
+
+function join() {
+    $("#modalA").fadeIn();
+}
+function popClose() {
+	$("#modalA").fadeOut();
+}
+function closePop() {
+	$("#opened").fadeOut();
+}
