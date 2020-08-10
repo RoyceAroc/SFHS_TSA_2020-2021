6c064e359562e030c3cbf3b08a405e2b75dd96fc
RoyceAroc <47615786+RoyceAroc@users.noreply.github.com>
2020-08-10 14:49:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -219,3 +219,12 @@ function popClose() {
 function closePop() {
 	$("#opened").fadeOut();
 }
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginRight = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginRight= "0";
+}
